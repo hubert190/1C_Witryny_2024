98d5dc17f84282ce74a2c26efb3a3eff3d9e5a5a
hubert190 <144451095+hubert190@users.noreply.github.com>
2024-09-10 15:05:55 +0200

    Add files via upload

---
+++ dd/sript.js
@@ -0,0 +1,7 @@
+ 
+    document.write("Wizytówka<br>");
+    document.write("Artur Mikoł<br>");
+    document.write("Specjalizacje: Bazy danych<br>");
+    document.write("Kontakt: <br>-exxxxxxd@emfme.fem <br>-2032830283<br>");
+    alert("Witaj tutaj możesz zobaczyć moją wizytówke!");
+    
