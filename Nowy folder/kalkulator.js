d5c0d5d5e730b03c34553dbf20e400b4fa115c6e
hubert190 <144451095+hubert190@users.noreply.github.com>
2024-09-17 14:54:29 +0200

    Add files via upload

---
+++ Nowy folder/kalkulator.js
@@ -0,0 +1,20 @@
+let liczba1;
+let liczba2;
+
+liczba1= prompt("podaj liczbe 1");
+liczba2= prompt("podaj liczbe 2");
+
+let wynik;
+
+
+wynik=liczba1 +liczba2;
+document.write(" wynik dodawania: " , wynik);
+
+wynik=liczba1*liczba2;
+document.write(" wynik mnożenia: " , wynik);
+
+wynik=liczba1-liczba2;
+document.write(" wynik odejmowania " , wynik);
+
+wynik=liczba1/liczba2;
+document.write(" wynik dzielenia: " , wynik);
